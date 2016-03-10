f3c755c633d7e812b47cb9c70ef0b1272d236448
S2366681 <s2366681@fwn-bborg-edu-74-5.fmnsedu.rug.nl>
2016-03-10 14:55:45 +0100

    first try at ex2

---
+++ lab4/stereoprakt/depthmap.m
@@ -0,0 +1,27 @@
+function I = depthmap(IL, IR, pw, ph, match)
+% pw     width patch
+% ph     height patch
+% match  matching method
+% shape  shape of the 
+
+Iwidth = length(IL(1,:,1))-pw+1;
+Iheight = length(IL(:,1,1))-ph+1;
+I = zeros(Iheight, Iwidth);
+
+wbar = waitbar(0,'Constructing depth map...');
+for ity = 1 : Iheight
+    for  itx = 1 : Iwidth
+        patchL = IL(ity:ity+ph-1, itx:itx+pw-1, :);
+        
+        stripR = IR(ity:ity+ph-1, 1 : itx+pw-1, :); 
+        rsimilarity = match(patchL(:,:,1), stripR(:,:,1));
+        gsimilarity = match(patchL(:,:,1), stripR(:,:,2));
+        bsimilarity = match(patchL(:,:,1), stripR(:,:,3));
+        
+        [~, xR] = max(rsimilarity + gsimilarity + bsimilarity);
+        
+        I(ity,itx) = itx - xR;
+    end
+    waitbar(ity/Iheight);
+end
+close(wbar);
