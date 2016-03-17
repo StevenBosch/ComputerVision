7e849e899cc1b2dcd254721bea715d660d6ecb1d
Steven <s.bosch.8@student.rug.nl>
2016-03-17 10:15:07 +0100

    bla

--- lab4/stereoprakt/xcorrdiff.m
+++ lab4/stereoprakt/xcorrdiff.m
@@ -1,4 +1,6 @@
 function similarities = xcorrdiff(patch, strip)
 
 corr = normxcorr2(patch, strip);
-similarities = corr(length(patch(:, 1)), :);
+first = length(patch(1, :));
+last = length(corr(1, :)) - length(patch(1,:)) + 1;
+similarities = corr(length(patch(:, 1)), first:last);
