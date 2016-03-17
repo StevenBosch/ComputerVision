5f6f33382183bf6056517e111ae3f24e336d6519
S2366681 <s2366681@fwn-bborg-edu-74-118.fmnsedu.rug.nl>
2016-03-17 11:41:43 +0100

    uncommented gaussian lines

--- lab4/stereoprakt/squareddiff.m
+++ lab4/stereoprakt/squareddiff.m
@@ -1,16 +1,16 @@
 function similarities = squareddiff(patch, strip)
 
 diffs = zeros(length(strip(1,:)) - length(patch(1,:)) + 1, 1);
-%win = gausswin(length(patch(1,:))) * gausswin(length(patch(:,1)))';
-%patch = im2double(patch) .* win; %remove if not gauss
-%strip = im2double(strip); %remove if not gauss
+win = gausswin(length(patch(1,:))) * gausswin(length(patch(:,1)))';
+patch = im2double(patch) .* win; %remove if not gauss
+strip = im2double(strip); %remove if not gauss
 
 % move it over all positions in the strip the patch has to be compared with
 for it1 = 1 : length(strip(1,:)) - (length(patch(1,:)) -1)
     
     % patch2 is the part of the strip overlapping with the patch
     patch2 = strip(:, it1:it1+length(patch(1,:))-1);
-    %patch2 = patch2.*win; %remove if not gauss
+    patch2 = patch2.*win; %remove if not gauss
     
     % calculate difference
     diffs(it1) = sum(sum((patch - patch2).^2));
