1e8be9dfa2348420bcbfa451ffce4eba9c79755e
S2366681 <s2366681@fwn-bborg-edu-74-117.fmnsedu.rug.nl>
2016-03-17 16:33:10 +0100

    final version

--- lab4/stereoprakt/squareddiff.m
+++ lab4/stereoprakt/squareddiff.m
@@ -1,9 +1,9 @@
 function similarities = squareddiff(patch, strip)
 
 diffs = zeros(length(strip(1,:)) - length(patch(1,:)) + 1, 1);
-%win = gausswin(length(patch(1,:))) * gausswin(length(patch(:,1)))';
-%patch = im2double(patch) .* win; %remove if not gauss
-%strip = im2double(strip); %remove if not gauss
+win = gausswin(length(patch(1,:))) * gausswin(length(patch(:,1)))';
+patch = im2double(patch) .* win; %remove if not gauss
+strip = im2double(strip); %remove if not gauss
 
 % move it over all positions in the strip the patch has to be compared with
 for it1 = 1 : length(strip(1,:)) - (length(patch(1,:)) -1)
